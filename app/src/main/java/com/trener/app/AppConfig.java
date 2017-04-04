8f32b18a54ff37e187bff1bf9bea03350510635d
Zajaczekk <zajaczek1910@gmail.com>
2017-04-04 17:18:34 +0200

    Initial commit

---
+++ app/src/main/java/com/trener/app/AppConfig.java
@@ -0,0 +1,11 @@
+package com.trener.app;
+
+public class AppConfig {
+	// Server user login url
+	//public static String URL_LOGIN = "http://149.202.92.150/projekt/login.php";
+	public static String URL_LOGIN = "http://kzajaczkowski.cba.pl/login.php";
+
+	// Server user register url
+	//public static String URL_REGISTER = "http://149.202.92.150/projekt/register.php";
+	public static String URL_REGISTER = "http://kzajaczkowski.cba.pl/register.php";
+}
